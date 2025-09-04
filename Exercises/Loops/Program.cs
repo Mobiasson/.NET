4950b1733cecf3bed053bcfe3d1209eedac40637
Mobiasson <mikaeltobiasson@hotmail.com>
2025-09-04 16:31:41 +0200

    Exercise3 for Loops

--- Exercises/Loops/Program.cs
+++ Exercises/Loops/Program.cs
@@ -8,16 +8,32 @@
             Console.WriteLine(i);
         }
     }
-    
-    /* 2. Jämna tal */    
+
+    /* 2. Jämna tal */
     static void Exercise2()
     {
-        for (int i = 0; i <= 30; i+=2) {
+        for (int i = 0; i <= 30; i += 2)
+        {
             Console.WriteLine(i);
         }
-
-        
     }
+
+    // 3. Var tredje "Hej"
+    static void Exercise3()
+    {
+        for (int i = 0; i <= 30; i++)
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine("Hej");
+            }
+            else
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+
     static void Main(string[] args)
     {
         while (true)
@@ -25,6 +41,7 @@
             Console.WriteLine("Write 'q' to exit");
             Console.WriteLine("1. Skriv tal 20 till 30");
             Console.WriteLine("2. Jämna tal");
+            Console.WriteLine("3. Var tredje 'Hej'");
             string? choice = Console.ReadLine();
             switch (choice)
             {
@@ -35,6 +52,9 @@
                     break;
                 case "2":
                     Exercise2();
+                    break;
+                case "3":
+                    Exercise3();
                     break;
                 case "q":
                     Console.WriteLine("Exiting program");
