9493c7d371083fce0c2d0b1f46f4c462ea250309
Mobiasson <mikaeltobiasson@hotmail.com>
2025-09-06 17:41:52 +0200

    Exercise 1 and 2 in indexing

--- Exercises/Indexering/Program.cs
+++ Exercises/Indexering/Program.cs
@@ -1,16 +1,34 @@
 ﻿class Indexing {
+	static void Exercise1() {
+		Console.WriteLine("Write any word");
+		string? input = Console.ReadLine();
+		foreach(char c in input) {
+			Console.WriteLine(c);
+		}
+	}
+	static void Exercise2() {
+		string[] stringsArray = { "noll", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio" };
+		Console.WriteLine("Write a number 0-9");
+		string? input = Console.ReadLine();
+		int value = StringToInt(input);
+		Console.WriteLine(stringsArray[value]);
+	}
 
 
 
 	static void Main(string[] args) {
 		while(true) {
 			Console.WriteLine("1. En bokstav på varje rad");
+			Console.WriteLine("2. Siffror till text");
 			string? choice = Console.ReadLine();
 			switch(choice) {
 				case "0":
 					break;
 				case "1":
 					Exercise1();
+					break;
+				case "2":
+					Exercise2();
 					break;
 				case "q":
 					Console.WriteLine("Exiting program");
@@ -22,3 +40,13 @@
 			}
 		}
 	}
+	static bool IsInt(string input) {
+		return int.TryParse(input, out _);
+	}
+	static int StringToInt(string input) {
+		if(int.TryParse(input, out int value)) {
+			return value;
+		}
+		return -1;
+	}
+}
