e5ca008b682dddf0e82e679839962bbac339ccc7
Mobiasson <mikaeltobiasson@hotmail.com>
2025-09-07 09:46:03 +0200

    Exercise4 in Indexing

--- Exercises/Indexering/Program.cs
+++ Exercises/Indexering/Program.cs
@@ -1,4 +1,5 @@
-﻿class Indexing {
+﻿#pragma warning disable CS8602
+class Indexing {
 	static void Exercise1() {
 		Console.WriteLine("Write any word");
 		string? input = Console.ReadLine();
@@ -8,18 +9,50 @@
 	}
 	static void Exercise2() {
 		string[] stringsArray = { "noll", "ett", "två", "tre", "fyra", "fem", "sex", "sju", "åtta", "nio" };
-		Console.WriteLine("Write a number 0-9");
-		string? input = Console.ReadLine();
-		int value = StringToInt(input);
-		Console.WriteLine(stringsArray[value]);
+		Console.WriteLine("Write a number 0-9:");
+		while(true) {
+			try {
+				string? input = Console.ReadLine();
+				if(!int.TryParse(input, out int value)) Console.WriteLine("This is not an integer! Try again! 0-9");
+				else Console.WriteLine(stringsArray[value]);
+			} catch(IndexOutOfRangeException) {
+				Console.WriteLine("Not in the range of 0-9. Try again!");
+			}
+		}
 	}
 
 
+	static void Exercise3() {
+		Console.WriteLine("Write anything you want.");
+		string? input = Console.ReadLine();
+		for(int i = input.Length - 1; i >= 0; i--) {
+			Console.WriteLine(input[i]);
+		}
+	}
+
+	static void Exercise4() {
+		Console.WriteLine("Write anything you want.");
+		while(true) {
+			string? input = Console.ReadLine();
+			NullStringChecker(input);
+			string[] vokaler = { "a", "e", "i", "o", "u", "y", "å", "ä", "ö" };
+			String newString = input;
+			for(int i = 0; i < vokaler.Length; i++) {
+				newString = newString.Replace(vokaler[i], "*");
+			}
+			Console.WriteLine($"New replaced string: {newString}");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+			break;
+		}
+	}
 
 	static void Main(string[] args) {
 		while(true) {
 			Console.WriteLine("1. En bokstav på varje rad");
 			Console.WriteLine("2. Siffror till text");
+			Console.WriteLine("3. Baklänges");
+			Console.WriteLine("4. Dölj vokaler");
 			string? choice = Console.ReadLine();
 			switch(choice) {
 				case "0":
@@ -29,6 +62,12 @@
 					break;
 				case "2":
 					Exercise2();
+					break;
+				case "3":
+					Exercise3();
+					break;
+				case "4":
+					Exercise4();
 					break;
 				case "q":
 					Console.WriteLine("Exiting program");
@@ -40,13 +79,27 @@
 			}
 		}
 	}
+
 	static bool IsInt(string input) {
 		return int.TryParse(input, out _);
 	}
+
 	static int StringToInt(string input) {
 		if(int.TryParse(input, out int value)) {
 			return value;
 		}
 		return -1;
+	}
+
+	static bool NullStringChecker(string? input) {
+		if(string.IsNullOrEmpty(input)) {
+			Console.WriteLine("String is null or empty. Try again!");
+			return false;
+		}
+		if(input.All(char.IsDigit)) {
+			Console.WriteLine("Cannot be a digit. Try again!");
+			return false;
+		}
+		return true;
 	}
 }
