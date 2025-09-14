edbb9514b85ea596cf2d3dcf90eeb8182d6c134c
Mobiasson <mikaeltobiasson@hotmail.com>
2025-09-14 23:15:09 +0200

    Added Labb1 in directory 'Assignments'

---
+++ Assignments/Labb1/Program.cs
@@ -0,0 +1,38 @@
+﻿class Labb {
+	static void Main(string[] args) {
+		string randomString = randomStringGen(30);
+		ulong sum = 0;
+		for(int i = 0; i < randomString.Length; i++) {
+			char currentIndex = randomString[i];
+			int nextIndex = randomString.IndexOf(currentIndex, i + 1);
+			if(nextIndex != -1 && letterChecker(randomString, i, nextIndex)) {
+				string digitString = string.Concat(randomString.Substring(i, nextIndex - i + 1).Where(char.IsDigit)); sum += ulong.Parse(digitString);
+				for(int j = 0; j < randomString.Length; j++) {
+					Console.ForegroundColor = (j >= i && j <= nextIndex) ? ConsoleColor.Red : ConsoleColor.Gray; Console.Write(randomString[j]);
+				}
+				Console.WriteLine();
+			}
+			Console.ResetColor();
+		}
+		Console.WriteLine($"The sum of everything is {sum}");
+	}
+
+	static string randomStringGen(int stringLength) {
+		Random rdn = new Random();
+		char[] charArr = new char[stringLength];
+		string digits = "0123456789";
+		string letters = "abcdefghijklmnopqrstuvwxyz";
+		for(int i = 0; i < charArr.Length; i++) if(rdn.NextDouble() < 0.9) charArr[i] = digits[rdn.Next(digits.Length)];
+			else charArr[i] = letters[rdn.Next(letters.Length)];
+		string longString = new String(charArr);
+		return longString;
+	}
+
+
+	static bool letterChecker(string input, int startIndex, int endIndex) {
+		for(int i = startIndex; i <= endIndex; i++) {
+			if(char.IsLetter(input[i])) return false;
+		}
+		return true;
+	}
+}
--- Exercises/Labb1/Program.cs
+++ Exercises/Labb1/Program.cs
@@ -1,72 +1,95 @@
-﻿class Program
-{
-	static void Exercise1()
-	{
+﻿#pragma warning disable CS8602
+#pragma warning disable CS8600
+
+class Program {
+	static void Exercise1() {
 		Console.WriteLine("Write sentence");
 		string? input = Console.ReadLine();
-		foreach (char c in input)
-		{
-			if (c == ' ') break;
+		foreach(char c in input) {
+			if(c == ' ') break;
 			Console.WriteLine(c);
 		}
 	}
 
-	static void Exercise2()
-	{
+	static void Exercise2() {
 		Console.WriteLine("Write anything you want.");
 		string? input = Console.ReadLine();
 		string[] split = input.Split(" ");
-		foreach (string str in split) Console.WriteLine(str);
+		foreach(string str in split) Console.WriteLine(str);
 	}
 
-	static void Exercise3()
-	{
+	static void Exercise3() {
 		string input = "Detta är uppgift 3";
 		char[] result = input.ToCharArray();
-		for (int i = 0; i < input.Length; i++) if (i % 2 == 0)
-			{
+		for(int i = 0; i < input.Length; i++) if(i % 2 == 0) {
 				result[i] = '*';
 			}
 		Console.WriteLine(result);
 	}
 
-	static void Exercise4()
-	{
-		Console.WriteLine("Hello world!");
-		string input = Console.ReadLine();
-		foreach (char c in input)
-		{
-			if (c == 'i') Console.ForegroundColor = ConsoleColor.Red;
-			else if (c == 'o') Console.ForegroundColor = ConsoleColor.Green;
+	static void Exercise4() {
+		string input = "Hello World";
+		foreach(char c in input) {
+			if(c == 'l') Console.ForegroundColor = ConsoleColor.Red;
+			else if(c == 'o') Console.ForegroundColor = ConsoleColor.Green;
 			else Console.ForegroundColor = ConsoleColor.Gray;
+			Console.Write(c);
 		}
+		Console.ResetColor();
+		Console.WriteLine();
 	}
 
-	static void Exercise5()
-	{
-		Console.WriteLine("Hello world!");
-		string input = Console.ReadLine();
-		int charCount = 0;
-		foreach (char c in input)
-		{
-			Console.WriteLine(c);
+	static void Exercise5() {
+		string input = "Hello world!";
+		for(int i = 0; i < input.Length; i++) {
+			if(i < input.Length - 1 && input[i] == input[i + 1]) {
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.Write(input[i]);
+				Console.Write(input[i + 1]);
+				i++;
+			} else {
+				Console.ForegroundColor = ConsoleColor.Gray;
+				Console.Write(input[i]);
+			}
 		}
+		Console.ResetColor();
+		Console.WriteLine();
+	}
+
+	static void Exercise6() {
+		string input = "How much wood would a woodchuck chuck if a woodchuck could chuck wood?";
+		string shortInput = "oo";
+			for(int i = 0; i < input.Length;i++) {
+					if(i < input.Length - 1 && input.Substring(i, 2) == shortInput){
+							Console.ForegroundColor = ConsoleColor.Green;
+							Console.Write(input[i]);
+							Console.Write(input[i + 1]);
+					} else {
+						Console.ForegroundColor = ConsoleColor.White;
+						Console.Write(input[i]);
+					}
+				}
+				Console.ResetColor();
+				Console.WriteLine();
+			}
+
+	static void Exercise7() {
+
 	}
 
 
-
-	static void Main(string[] args)
-	{
-		while (true)
-		{
+	static void Main(string[] args) {
+		while(true) {
+			Console.WriteLine();
 			Console.WriteLine("1. Bokstav för bokstav - fram till space");
 			Console.WriteLine("2. Bokstav för bokstav - radbyte för space");
 			Console.WriteLine("3. Bokstav för bokstav - varannan stjärna");
 			Console.WriteLine("4. Bokstav för bokstav - gröna o, röda I");
 			Console.WriteLine("5. Bokstav för bokstav - dubbla med grön färg");
+			Console.WriteLine("6. Bokstav för bokstav - grön substring");
+			Console.WriteLine("7. Bokstav för bokstav - grön substring 2");
 			string? choice = Console.ReadLine();
-			switch (choice)
-			{
+			switch(choice) {
 				case "0":
 					break;
 				case "1":
@@ -83,6 +106,12 @@
 					break;
 				case "5":
 					Exercise5();
+					break;
+				case "6":
+					Exercise6();
+					break;
+				case "7":
+					Exercise7();
 					break;
 				case "q":
 					Console.WriteLine("Exiting program");
