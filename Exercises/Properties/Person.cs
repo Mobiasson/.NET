cd44eff7c8770db8097f9c468662cf543ed959ff
Mobiasson <mikaeltobiasson@hotmail.com>
2025-09-27 15:14:45 +0200

    Exercises in properties

--- Exercises/Properties/Person.cs
+++ Exercises/Properties/Person.cs
@@ -3,48 +3,49 @@
 #pragma warning disable CS8601
 
 namespace Exercises.Person {
-class Person {
-	private string _firstName;
+	class Person {
+		private string _firstName;
 
-	public Person(string firstName) {
-		_firstName = firstName;
-	}
+		public Person(string firstName) {
+			_firstName = firstName;
+		}
 
-	public string FirstName {
-		get{return _firstName;}
-		set{_firstName = value;}
-	}
+		public string FirstName {
+			get { return _firstName; }
+			set { _firstName = value; }
+		}
 
-	public string FullName {
-		get{return _firstName + LastName;}
-	}
+		public string FullName {
+			get { return _firstName + LastName; }
+		}
 
-	public string LastName {get;set;}
+		public string LastName { get; set; }
 	}
 }
 
 namespace Exercises.StepCounter {
-class StepCounter {
-	public int steps;
+	class StepCounter {
 
-	public int Steps {
-		get{return steps;}
-	}
+		public int steps;
 
-	public void Step() {
-		steps++;
-		Console.WriteLine($"I have taken {steps} steps");
-	}
+		public int Steps {
+			get { return steps; }
+		}
 
-	public void Reset() {
-		steps = 0;
-		Console.WriteLine("I have reseted to 0");
+		public void Step() {
+			steps++;
+			Console.WriteLine($"I have taken {steps} steps");
+		}
+
+		public void Reset() {
+			steps = 0;
+			Console.WriteLine("I have reseted to 0");
 		}
 	}
 }
 
 namespace Exercises.Car {
-	class Car{
+	class Car {
 		private string model;
 		private double price;
 		private string color;
@@ -73,3 +74,46 @@ namespace Exercises.Car {
 		}
 	}
 }
+
+namespace Exercises.Water {
+	class Water {
+		private bool isGlasFull = false;
+		private bool isGlasBroken = false;
+
+		public void FillGlas() {
+			if(!isGlasFull == true) {
+				isGlasFull = true;
+				Console.WriteLine("I just filled the glas!");
+			} else Console.WriteLine("The glas is already full!");
+
+		}
+
+		public void EmptyGlas() {
+			if(isGlasFull == true) {
+				isGlasFull = false;
+				Console.WriteLine("I just emptied the glas!");
+			} else Console.WriteLine("Glas is already empty!");
+		}
+
+		public void breakGlas() {
+			if(isGlasBroken == false) Console.WriteLine($"The glas is broken and cannot be filled anymore"); else isGlasBroken = true;
+			if(isGlasFull) {
+				isGlasFull = false;
+				Console.WriteLine("Glas is broken");
+			} else Console.WriteLine("Glas breaking");
+		}
+	}
+}
+
+namespace Exercises.Colors {
+	class Colors {
+	static Random rnd = new Random();
+	public double blue = Math.Round(rnd.NextDouble() * 100, 2);
+	public double red = Math.Round(rnd.NextDouble() * 100, 2);
+
+
+
+		}
+	}
+
+
--- Exercises/Properties/Program.cs
+++ Exercises/Properties/Program.cs
@@ -1,25 +1,42 @@
 ﻿#pragma warning disable CS8618
+#pragma warning disable CS8019
 
 using Exercises.Person;
 using Exercises.StepCounter;
 using Exercises.Car;
+using Exercises.Water;
+using Exercises.Colors;
 
 class Program {
-	static void Main(string[]args) {
+	static void Main(string[] args) {
 
-	Person person1 = new Person("Mikael");
-	person1.LastName = "Ros Tobiasson";
-	/* Console.WriteLine($"{person1.FirstName} {person1.LastName}"); */
-	Console.WriteLine($"{person1.FullName}");
+		/* Person person1 = new Person("Mikael");
+		person1.LastName = "Ros Tobiasson";
+		Console.WriteLine($"{person1.FirstName} {person1.LastName}");
+		Console.WriteLine($"{person1.FullName}");
 
-	/* StepCounter sc = new StepCounter();
-	for(int i = 0; i < 1000; i++) {
+		StepCounter sc = new StepCounter();
+		for(int i = 0; i < 1000; i++) {
 			sc.Step();
-	} */
+		}
 
-	Car car = new Car("BMW", 1500000, "Black");
-	car.HalfPrice();
-	Console.WriteLine(car);
+		Car car = new Car("BMW", 1500000, "Black");
+		car.HalfPrice();
+		Console.WriteLine(car);
+
+		Water waterGlas = new Water();
+		waterGlas.FillGlas();
+		waterGlas.FillGlas();
+		waterGlas.EmptyGlas();
+		waterGlas.EmptyGlas();
+		waterGlas.breakGlas();
+		waterGlas.FillGlas(); */
+
+
+		Colors colors = new Colors();
+		colors.blue = 100 - colors.red;
+		Console.WriteLine($"Color blue is: {colors.blue}");
+		Console.WriteLine($"Color red is: {colors.red}");
 
 
 
