786a949f4e27a7571d55ed426328d410af4a3092
Mobiasson <mikaeltobiasson@hotmail.com>
2025-10-01 11:21:07 +0200

    Exercise 4,5 in Inheritance

--- Exercises/Inheritance/Inheritance.cs
+++ Exercises/Inheritance/Inheritance.cs
@@ -5,15 +5,21 @@ enum Brand {
 }
 
 enum Color {
-	Red, Black, Violet, Green ,Blue, White
+	Red, Black, Violet, Green, Blue, White
 }
 
 class Vehicle {
-	private Brand _brand;
-	private Color _color;
+	Random rnd = new Random();
+	public Brand _brand { get; set; }
+	public Color _color { get; set; }
+	public Size Dimensions { get; set; }
 
 	public Vehicle() {
-
+		Dimensions = new Size {
+			_length = 1.5 + rnd.NextDouble() * 2,
+			_width = 1 + rnd.NextDouble() * 2,
+			_height =  0.5 + rnd.NextDouble() * 2
+		};
 	}
 
 	public Vehicle(Brand brand, Color color) {
@@ -23,5 +29,35 @@ class Vehicle {
 
 	public override string ToString() {
 		return $"A {_color} {_brand}";
+	}
+
+}
+
+class Car : Vehicle {
+	public string _model;
+
+
+	public string Model { get => _model; set => _model = value; }
+
+	public Car(Brand brand, Color color, string model) {
+		_brand = brand;
+		_color = color;
+		_model = model;
+	}
+
+	public override string ToString() {
+		return $"A {_color} {Dimensions._length:f2}m {_model} from {_brand}";
+	}
+}
+
+struct Size {
+	public double _length { get; set; }
+	public double _width { get; set; }
+	public double _height { get; set; }
+
+	public Size(double length, double width, double height) {
+		_length = length;
+		_width = width;
+		_height = height;
 	}
 }
--- Exercises/Inheritance/Program.cs
+++ Exercises/Inheritance/Program.cs
@@ -1,6 +1,7 @@
 ﻿class Program {
 	static void Main(string[] args) {
 		Vehicle myVehicle = new Vehicle(Brand.Toyota, Color.White);
-		Console.WriteLine(myVehicle);
+		// Console.WriteLine(myVehicle);
+		Console.WriteLine(new Car(Brand.Toyota, Color.White, "Yaris"));
 	}
 }
