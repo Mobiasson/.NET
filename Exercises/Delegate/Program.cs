98cf3f0f3d5f0f1ede4a28c30d66b4c33ec7439d
Mobiasson <mikaeltobiasson@hotmail.com>
2025-10-25 07:54:14 +0200

    Added WPF exercises and added commands to helper.txt

--- Exercises/Delegate/Program.cs
+++ Exercises/Delegate/Program.cs
@@ -1,5 +1,6 @@
 ﻿class Program
 {
+	// 1. Deklarera en egen delegat-typ
     static string FullName(string firstName, string lastName)
     {
         Func<string, string, string> myFunc = (f, l) => $"{f} {l}";
@@ -8,7 +9,7 @@
         return result;
     }
 
-
+	// 2. Använd generiskt delegat
     static string GenericDelegate(string firstName, string lastName)
     {
         Func<string, string, string> myFunc = (firstName, lastName) => $"{firstName} {lastName}";
@@ -17,6 +18,7 @@
         return result;
     }
 
+	// 3. Metod som tar delegat som inparameter
 	static void DelegateParameter(Func<string, string, string> funcArray) {
 	string[] firstNames = {"Mikael", "Signe", "Abo"};
 	string[] lastNames = {"Ros Tobiasson", "Blix Herrlander", "Blix Tobiasson"};
@@ -25,17 +27,23 @@
 		}
 	}
 
+	// 4. Skriv ut summan av två värden
 	static void SumWithLambda(int firstNum, int secondNum) {
 	Func<int, int, int> myFunc = (f, s) => f + s;
 	int result = myFunc.Invoke(firstNum, secondNum);
 	Console.WriteLine(result);
 	}
 
-
+	//5. Returnera summan av två värden
+	static int SumTwoValues(int i, int j) {
+	int sum = 0;
+	Func<int, int, int> myFunc = (i, j) =>
+		return sum;
+	}
 
     static void Main(string[] args) {
 	DelegateParameter((firstName , lastName) => $"{firstName} {lastName}");
 	SumWithLambda(10, 5);
-
+	DelegateParameter((firstName, lastName) => $"{firstName} {lastName}");
     }
 }
