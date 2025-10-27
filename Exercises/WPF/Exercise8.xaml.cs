bab0ab2ec2f22876a742cf32378fc2d4801db537
Mobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2025-10-27 23:50:31 +0100

    Added .vs to .gitignore and XAML exercises

---
+++ Exercises/WPF/Exercise8.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for Exercise8.xaml
+    /// </summary>
+    public partial class Exercise8 : Window
+    {
+        public Exercise8()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(firstName.Text) ||
+                string.IsNullOrWhiteSpace(lastName.Text) ||
+                string.IsNullOrWhiteSpace(email.Text))
+                return;
+
+            list.Items.Add($"{firstName.Text} {lastName.Text} - {email.Text}");
+            firstName.Clear();
+            lastName.Clear();
+            email.Clear();
+        }
+    }
+}
