81054f6928ce44515ba234b4589a3a322d45a49d
Mobiasson <117571587+MRosTobiasson@users.noreply.github.com>
2025-10-28 09:50:38 +0100

    Added Exercise 1 & 2 in XPF_Controller

--- Exercises/WpfController/Exercise1.xaml.cs
+++ Exercises/WpfController/Exercise1.xaml.cs
@@ -11,14 +11,17 @@ using System.Windows.Shapes;
 
 namespace XAML
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void click_Click(object sender, RoutedEventArgs e) {
+            int count = int.Parse(click.Content.ToString());
+            click.Content = (count + 1).ToString();
+
         }
     }
 }
---
+++ Exercises/WpfController/Exercise2.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace XAML {
+    public partial class Exercise2 : Window {
+        public Exercise2() {
+            InitializeComponent();
+        }
+
+        private void increment_button_Click(object sender, RoutedEventArgs e) {
+            if(int.TryParse(counter.Content.ToString(), out int count))
+                counter.Content = (count + 1).ToString();
+        }
+
+        private void decrease_button_Click(object sender, RoutedEventArgs e) {
+            if(int.TryParse(counter.Content.ToString(), out int count))
+                counter.Content = (count - 1).ToString();
+        }
+    }
+}
